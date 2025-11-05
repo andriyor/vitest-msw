151c34271419daa0a5aa272bd257ed6b9de0d608
Andrii Oriekhov <andriyorehov@gmail.com>
2025-11-05 21:28:26 +0200

    update

--- setupTests.js
+++ setupTests.js
@@ -1,8 +1,8 @@
 import nock from "nock";
-
-nock('https://httpun.com')
-    .get('/status/400')
-    .reply(200, {
-        id: "15d42a4d-1948-4de4-ba78-b8a893feaf45",
-        firstName: "John",
-    })
+//
+// nock('https://httpun.com')
+//     .get('/status/400')
+//     .reply(200, {
+//         id: "15d42a4d-1948-4de4-ba78-b8a893feaf45",
+//         firstName: "John",
+//     })
